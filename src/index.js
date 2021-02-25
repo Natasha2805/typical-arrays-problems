999b56e7565d686448483b02715a084c7b9835f8
natasha <nataliasolox@gmail.com>
2021-02-25 21:00:46 +0300

    feat: add part

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(Array.isArray(array) == false || array.length == 0) {return 0};
+  return  Math.min(...array);
+
 }
 
 exports.max = function max (array) {
-  return 0;
+    if(Array.isArray(array) == false || array.length == 0) {return 0};
+    return  Math.max(...array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if(Array.isArray(array) == false || array.length == 0) {return 0};
+        let average = 0;
+    if (array.length > 0)
+    {
+        let sum = 0;
+        for (let i = 0; i < array.length; i++) {
+             sum += array[i];
+        }
+        average = sum / array.length;
+    }
+    return average
+   
 }
